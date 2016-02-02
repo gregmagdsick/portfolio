e09293fa3d2bb9fbe4505cc938a5cc8ada390d3c
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-01 22:01:48 -0800

    finished constructor

---
+++ script/app.js
@@ -0,0 +1,9 @@
+var portfolioElements = [];
+
+function Portfolios (opts) {
+  this.title = opts.title;
+  this.repoUrl = opts.repoUrl;
+  this.deployUrl = ops.deployUrl;
+  this.finishedOn = opts.finishedOn;
+  this.snippet = opts.snippet;
+};
