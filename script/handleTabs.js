d4383c7660bb5aafb580909ccd178680827217b6
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-03 16:29:38 -0800

    added name to index.html

--- script/handleTabs.js
+++ script/handleTabs.js
@@ -3,9 +3,7 @@ var handleTabs = {};
 handleTabs.toggleSections = function() {
   $('.tabs').on('click', 'li', function(e) {
     $('.part-content').hide();
-    console.log($(this).data('content'));
     $('#' + $(this).data('content') + '').show();
-    // $(this).parent('ul').hide();
   });
   $('.tabs .tab:first').click();
 };
