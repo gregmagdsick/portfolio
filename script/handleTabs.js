d10fac3cd646c10e137f721f0f56649660fb28e5
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-11 14:56:42 -0800

    fixed issue#18, handelTabs.js

--- script/handleTabs.js
+++ script/handleTabs.js
@@ -1,11 +1,14 @@
-var handleTabs = {};
+(function (module) {
+  var handleTabs = {};
 
-handleTabs.toggleSections = function() {
-  $('.tabs').on('click', 'li', function(e) {
-    $('.part-content').hide();
-    $('#' + $(this).data('content') + '').show();
-  });
-  $('.tabs .tab:first').click();
-};
+  handleTabs.toggleSections = function() {
+    $('.tabs').on('click', 'li', function(e) {
+      $('.part-content').hide();
+      $('#' + $(this).data('content') + '').show();
+    });
+    $('.tabs .tab:first').click();
+  };
 
-handleTabs.toggleSections();
+  handleTabs.toggleSections();
+  module.handleTabs = handleTabs;
+}(window));
