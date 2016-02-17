8147224ca2178bfda69c14df9b4ed0c8c0018406
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-16 21:58:24 -0800

    updated node server, made sure was using client side routing

---
+++ script/githubtoken.js
@@ -0,0 +1,1 @@
+var gitubToken = '0fdf0a9cd972ca1f9dcfcac276763617bac53425';
--- script/handleTabs.js
+++ script/handleTabs.js
@@ -1,14 +1,14 @@
-(function (module) {
-  var handleTabs = {};
-
-  handleTabs.toggleSections = function() {
-    $('.tabs').on('click', 'li', function(e) {
-      $('.part-content').hide();
-      $('#' + $(this).data('content') + '').show();
-    });
-    $('.tabs .tab:first').click();
-  };
-
-  handleTabs.toggleSections();
-  module.handleTabs = handleTabs;
-}(window));
+// (function (module) {
+//   var handleTabs = {};
+//
+//   handleTabs.toggleSections = function() {
+//     $('.tabs').on('click', 'li', function(e) {
+//       $('.part-content').hide();
+//       $('#' + $(this).data('content') + '').show();
+//     });
+//     $('.tabs .tab:first').click();
+//   };
+//
+//   handleTabs.toggleSections();
+//   module.handleTabs = handleTabs;
+// }(window));
